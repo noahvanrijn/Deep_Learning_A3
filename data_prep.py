0fbb59dcfb9ac2a0d8f4cef7cda38a4fc8f3bb06
florencecornelissen <f.y.cornelissen@student.vu.nl>
2023-12-13 01:03:24 +0100

    Different pad_and_convert, next word looks like it + Q7 begin

---
+++ data_prep.py
@@ -0,0 +1,15 @@
+from data_rnn import load_imdb, gen_sentence
+import numpy as np
+import torch 
+
+# Load the data
+(x_train, y_train), (x_val, y_val), (i2w, w2i), numcls = load_imdb(final=False)
+
+# Function to pad and convert the batch
+def pad_and_convert(batch):
+    max_len = max([len(x) for x in batch])
+    batch = [x + [w2i['.pad']] * (max_len - len(x)) for x in batch]
+    batch = torch.tensor(batch, dtype=torch.long)
+    return batch
+
+
